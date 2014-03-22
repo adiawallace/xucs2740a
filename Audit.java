0ae41dc056e340973ee8da6679f0cb965ea7fe7c
Andrea Edwards <aedwards@xula.edu>
2014-03-22 09:59:30 -0500

    Audit.java is Andrea's start

---
+++ Audit.java
@@ -0,0 +1,20 @@
+/*
+	This is a degree audit program.  Given a transcript and a curriculum,
+	this program displays the next best courses a student should take.
+
+	@author		Software Development Course (CPSC 2740)
+	@version	Spring 2014
+*/
+
+
+public class Audit {
+	public static void main(String[] args) {
+		System.out.println("\n\nWelcome to our Degree Audit Program!\n\n");
+
+		System.out.println("\nI need to read the user's transcript.");
+		System.out.println("\nI need to read the user's curriculum.");
+		System.out.println("\nI need to perform a diff on the two files.");
+
+	}
+
+}
